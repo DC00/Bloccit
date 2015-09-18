a3dbdee41c7713df712780008ffca6114a575ee2
DC00 <djc4ku@virginia.edu>
2015-09-18 02:46:48 -0400

    progress on assignment 31

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,11 @@
 class Post < ActiveRecord::Base
   has_many :comments
+
+  def censor_posts
+    @Post.all.each_with_index do |item, index|
+      if index % 5 == 0
+        @Post.at(index).title = "CENSORED"
+      end
+    end
+  end
 end
