149f535d543e524fe99358df58bbfe2e19e39d80
DC00 <djc4ku@virginia.edu>
2016-01-14 23:18:24 -0500

    completed checkpoint on comments

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   resources :sessions, only: [:new, :create, :destroy]
 
+  #  /posts/:post_id/comments
+  resources :posts, only: [] do
+    resources :comments, only: [:create, :destroy]
+  end
+
   get 'about' => 'welcome#about'
   root to: 'welcome#index'
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe Post, :type => :model do
   it { should validate_presence_of(:user) }
   # it { should validate_presence_of(:title).is_at_least(5) }
   # it { should validate_presence_of(:body).is_at_least(20) }
-
+  it { should have_many(:comments) }
 
 
   describe "attributes" do
