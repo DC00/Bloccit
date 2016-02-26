262e60ba9ea14893559bb77093b4af4a2c94b561
DC00 <djc4ku@virginia.edu>
2016-02-25 23:50:49 -0500

    completed voting checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160212085326) do
+ActiveRecord::Schema.define(version: 20160225192641) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 20160212085326) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
