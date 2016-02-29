e543209ff913ce8012c978749b1d54b963ba4d6f
DC00 <djc4ku@virginia.edu>
2016-02-29 01:05:15 -0500

    working on assignment for favorites. Error: http://stackoverflow.com/questions/24865596/rails-4-sendgrid-integration-giving-error-unauthenticated-senders-not-allowed

--- app/mailers/favorite_mailer.rb
+++ app/mailers/favorite_mailer.rb
@@ -2,14 +2,24 @@ class FavoriteMailer < ApplicationMailer
   default from: "djc4ku@virginia.edu"
 
   def new_comment(user, post, comment)
-    headers["Message-ID"] = "comments/#{comment.id}@bloccit.com"
-    headers["In-Reply-To"] = "post/#{post.id}@bloccit.com"
-    headers["References"] = "post/#{post.id}@bloccit.com"
+    headers["Message-ID"] = "comments/#{comment.id}@bloccit-uva.herokuapp.com"
+    headers["In-Reply-To"] = "post/#{post.id}@bloccit-uva.herokuapp.com"
+    headers["References"] = "post/#{post.id}@bloccit-uva.herokuapp.com"
 
     @user = user
     @post = post
     @comment = comment
 
     mail(to: user.email, subject: "New comment on #{post.title}")
+  end
+
+  def new_post(post)
+    headers["Message-ID"] = "posts/#{post.id}@bloccit-uva.herokuapp.com"
+    headers["In-Reply-To"] = "post/#{post.id}@bloccit-uva.herokuapp.com"
+    headers["References"] = "post/#{post.id}@bloccit-uva.herokuapp.com"
+
+    @post = post
+
+    mail(to: post.user.email, subject: "You're following, #{post.title}!")
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,6 +9,7 @@ class Post < ActiveRecord::Base
  
 
   after_create :create_vote
+  after_create :create_favorite
   default_scope { order('rank DESC') }
   scope :ordered_by_title, -> { order('title DESC')  }
   scope :ordered_by_reverse_created_at, -> { order('created_at ASC') } 
@@ -40,5 +41,10 @@ class Post < ActiveRecord::Base
 
   def create_vote
     user.votes.create(value: 1, post: self)
+  end
+
+  def create_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 end
