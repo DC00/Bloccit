46b9e6ef71232384217c4a26c3d7c4c46c2d1841
DC00 <djc4ku@virginia.edu>
2016-03-16 02:20:27 -0400

    working on last cp

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,6 +1,6 @@
 class Api::V1::UsersController < Api::V1::BaseController
   before_action :authenticate_user
-  before_action :authorized_user
+  before_action :authorize_user
 
   # finds a user based on id. Renders user object as json
   # and returns http 200
@@ -13,6 +13,14 @@ class Api::V1::UsersController < Api::V1::BaseController
   def index
     users = User.all
     render json: users.to_json, status: 200
+  end
+
+  def update
+
+  end
+
+  def create
+
   end
 
 end
--- spec/api/v1/controllers/users_controller_spec.rb
+++ spec/api/v1/controllers/users_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  let(:my_ufser) { create(:user) }
+  let(:my_user) { create(:user) }
 
   # Return 401 error on index and show if request is not authenticated
   context "unauthenticated users" do
@@ -12,63 +12,88 @@ RSpec.describe Api::V1::UsersController, type: :controller do
 
     it "GET show reutrns http unauthenticated" do
       get :show, id: my_user.id
-      expect(respnose).to have_http_status(401)
+      expect(response).to have_http_status(401)
     end
 
-    # Return a 403 if users are unauthenticated and unauthorized
-    context "authenticated and unauthorized users" do
-      before do
-        controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    # Expect unauthenticated requests to user update and create to be 401 unauthorized
+    it "PUT update returns http unauthenticated" do
+        new_user = build(:user)
+        put :update, id: my_user.id, user: { name: new_user.name, email:  new_user.email, password: new_user.password }
+        expect(response).to have_http_status(401)
+    end
+
+    it "POST create returns http unauthenticated" do
+        new_user = build(:user)
+        post :create, user: { name: new_user.name, email: new_user.email, password: new_user.password }
+        expect(response).to have_http_status(401)
+    end
+  end
+  # Return a 403 if users are unauthenticated and unauthorized
+  context "authenticated and unauthorized users" do
+    before do
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    end
+
+    it "GET index returns http forbidden" do
+      get :index
+      expect(response).to have_http_status(403)
+    end
+
+    it "GET show returns http forbidden" do
+      get :show, id: my_user.id
+      expect(response).to have_http_status(403)
+    end
+
+    # Expect non-admin users on update and create to be 403 Forbidden
+    it "PUT update returns http forbidden" do
+      new_user = build(:user)
+      put :update, id: my_user.id, user: { name: new_user.name, email: new_user.email, password: new_user.password }
+      expect(response).to have_http_status(403)
+    end
+
+    it "POST create returns http forbidden" do
+      new_user = build(:user)
+      post :create, user: { name: new_user.name, email: new_user.email, password: new_user.password }
+      expect(response).to have_http_status(403)
+    end
+  end
+
+  context "authenticated and authorized users" do
+    before do
+      my_user.admin!
+      controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    end
+
+    describe "GET index" do
+      before { get :index }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
       end
 
-      it "GET index returns http forbidden" do
-        get :index
-        expect(response).to have_http_status(403)
+      # Test that response.content_type is json. Content type is header added by API
+      it "returns json content type" do
+        expect(response.content_type).to eq("application/json")
       end
 
-      it "GET show returns http forbidden" do
-        get :show, id: my_user.id
-        expect(response).to have_http_status(403)
+      it "returns my_user serialized" do
+        expect(response.body).to eq([my_user].to_json)
       end
     end
 
-    context "authenticated and authorized users" do
-      before do
-        my_user.admin!
-        controller.request.env['HTTP_AUTHORIZATION'] = ActionController::HttpAuthentication::Token.encode_credentials(my_user.auth_token)
+    describe "GET show" do
+      before { get :show, id: my_user.id }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
       end
 
-      describe "GET index" do
-        before { get :index }
-
-        it "returns http success" do
-          expect(response).to have_http_status(:success)
-        end
-
-        # Test that response.content_type is json. Content type is header added by API
-        it "returns json content type" do
-          expect(response.content_type).to eq("application/json")
-        end
-
-        it "returns my_user serialized" do
-          expect(response.body).to eq([my_user].to_json)
-        end
+      it "returns json content type" do
+        expect(response.content_type). to eq 'application/json'
       end
 
-      describe "GET show" do
-        before { get :show, id: my_user.id }
-
-        it "returns http success" do
-          expect(response).to have_http_status(:success)
-        end
-
-        it "returns json content type" do
-          expect(response.content_type). to eq 'application/json'
-        end
-
-        it "returns my_user serialized" do
-          expect(response.body).to eq(my_user.to_json)
-        end
+      it "returns my_user serialized" do
+        expect(response.body).to eq(my_user.to_json)
       end
     end
   end
